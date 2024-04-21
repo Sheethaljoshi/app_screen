da4735a5c98f7c0eb388593b8c133cf0112ddd19
Sheethal <sh33thal24@gmail.com>
2024-04-21 13:50:10 +0530

    made tabs screen

--- category_app/lib/screens/meals_details.dart
+++ category_app/lib/screens/meals_details.dart
@@ -12,28 +12,56 @@ class MealsDetailsScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text(meal.title),
         ),
-        body:Column(children: [
-          Image.network(
-          meal.imageUrl,
-          width: double.infinity,
-          height: 300,
-          fit: BoxFit.cover,
-        ), 
-        const SizedBox(height: 14,),
-        Text('Ingredients', style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Theme.of(context).colorScheme.onBackground),),
-        const SizedBox(height: 14,),
-        for(final ingredient in meal.ingredients)
-          Text(ingredient, textAlign: TextAlign.left,style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Theme.of(context).colorScheme.onPrimary, ),),
-        const SizedBox(height: 24,),
-        Text('Instructions', style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Theme.of(context).colorScheme.onBackground),),
-        const SizedBox(height: 14,),
-        for(final step in meal.steps)
-          Padding(padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-          child: Text(step, textAlign: TextAlign.center,style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Theme.of(context).colorScheme.onPrimary, ),),
-       ),
-        const SizedBox(height: 24,)
-          
-
-        ],));
+        body: ListView( 
+  children: [
+    Image.network(
+      meal.imageUrl,
+      width: double.infinity,
+      height: 300,
+      fit: BoxFit.cover,
+    ), 
+    const SizedBox(height: 14,),
+    Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 2),  // Change the value as needed
+      child: Text(
+        'Ingredients', 
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.amber),
+      ),
+    ),
+    const SizedBox(height: 14,),
+    for(final ingredient in meal.ingredients)
+      Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 2),  // Change the value as needed
+        child: Text(
+          ingredient, 
+          textAlign: TextAlign.left,
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Theme.of(context).colorScheme.onBackground, ),
+        ),
+      ),
+    const SizedBox(height: 24,),
+    Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 2),  // Change the value as needed
+      child: Text(
+        'Instructions', 
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.amber),
+      ),
+    ),
+    const SizedBox(height: 14,),
+    for(final step in meal.steps)
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 3, vertical: 8),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 2),  // Change the value as needed
+          child: Text(
+            step, 
+            textAlign: TextAlign.left,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Theme.of(context).colorScheme.onBackground, ),
+          ),
+        ),
+      ),
+    const SizedBox(height: 24,)
+  ],
+)
+);
   }
 }
